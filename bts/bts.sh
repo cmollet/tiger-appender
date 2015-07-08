3563ac7a77cab710feefe83fcc932c88562bb950
Cory Mollet <cory@corymollet.com>
2015-07-08 08:14:54 -0500

    BTS needs some love too

---
+++ bts/bts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -r -l 1 -nd -A .zip http://www.rita.dot.gov/bts/sites/rita.dot.gov.bts/files/publications/national_transportation_atlas_database/2013/points.html
+wget -r -l 1 -nd -A .zip http://www.rita.dot.gov/bts/sites/rita.dot.gov.bts/files/publications/national_transportation_atlas_database/2013/polygon.html
